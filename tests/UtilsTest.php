7b761a4fcce7e9ffcf87c272cbc7ebad02cbbd94
Darkon <9017059@gmail.com>
2023-08-05 14:48:29 +0300

    another lint fix

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -3,16 +3,17 @@
 namespace Hexlet\Phpunit\Tests;
 
 use PHPUnit\Framework\TestCase;
+
 use function Hexlet\Phpunit\Utils\reverseString;
 
 class UtilsTest extends TestCase
 {
 
-	public function testReverse(): void
-	{
-		$this->assertEquals('', reverseString(''));
+    public function testReverse(): void
+    {
+        $this->assertEquals('', reverseString(''));
 
-		$this->assertEquals('olleh', reverseString('hello'));
-	}
+        $this->assertEquals('olleh', reverseString('hello'));
+    }
 
 }
